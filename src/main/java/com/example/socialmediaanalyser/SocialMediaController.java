3452c3bf9935197e74750a1349f0e4e117638814
Ridge19 <144869894+Ridge19@users.noreply.github.com>
2023-09-20 16:46:02 +1000

    - added new window - Added quit button on login page

--- src/main/java/com/example/socialmediaanalyser/SocialMediaController.java
+++ src/main/java/com/example/socialmediaanalyser/SocialMediaController.java
@@ -1,7 +1,11 @@
 package com.example.socialmediaanalyser;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 
@@ -18,6 +22,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
 
 public class SocialMediaController {
 
@@ -53,14 +61,47 @@ public class SocialMediaController {
 
 
     @FXML
-    void SignIn(ActionEvent event) {
+    public void SignIn(ActionEvent event) throws IOException {
         System.out.println("Test");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(SocialMedia.class.getResource("Main-Page.fxml"));
+
+        Parent main = FXMLLoader.load(getClass().getResource("Main-Page.fxml"));
+
+        Stage scene = new Stage();
+
+        scene.setTitle("Social Media Manager");
+
+        scene.setScene(scene.getScene());
+        scene.show();
+    }
+
+    public void Back(ActionEvent event) throws IOException {
+        System.out.println("Going back");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(SocialMedia.class.getResource("LoginPage.fxml"));
+
+        Parent main = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+
+        Stage scene = new Stage();
+
+        scene.setTitle("Social Media Manager - Login");
+
+        scene.setScene(scene.getScene());
+        scene.show();
     }
 
     @FXML
     void CreateAccount(ActionEvent event) {
         System.out.println("Creating account");
     }
+
+    @FXML
+    void Quit(ActionEvent event) {
+        System.out.println("Quitting");
+        Platform.exit();
+    }
+
 
 }
 
