9c770bb4f099f652bfb0fe250a6746f64f8d1ee1
Ridge19 <144869894+Ridge19@users.noreply.github.com>
2023-09-22 00:50:32 +1000

    - further enhancements to database connection - added "Login" database table to store username and passwords

--- src/main/java/com/example/socialmediaanalyser/CreateTable.java
+++ src/main/java/com/example/socialmediaanalyser/CreateTable.java
@@ -6,15 +6,16 @@ import java.sql.Statement;
 
 public class CreateTable {
     public static void main(String[] args) {
-        final String TABLE_NAME = "Student";
+        final String TABLE_NAME = "Login";
 
         try (Connection con = DatabaseConnection.getConnection();
              Statement stmt = con.createStatement();) {
             stmt.executeUpdate("CREATE TABLE IF NOT EXISTS " + TABLE_NAME
                     + "(id INT NOT NULL,"
-                    + "student_number VARCHAR(8) NOT NULL,"
-                    + "first_name VARCHAR(20) NOT NULL,"
-                    + "last_name VARCHAR(20) NOT NULL,"
+                    + "UserName VARCHAR(20) NOT NULL,"
+                    + "Password VARCHAR(20) NOT NULL,"
+                    + "FirstName VARCHAR(20) NOT NULL,"
+                    + "LastName VARCHAR(20) NOT NULL,"
                     + "PRIMARY KEY (id))");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
